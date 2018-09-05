8788b489549af3fbee6da6b0044f2b4ed031e850
karldiab <karl@karldiab.com>
2018-09-05 15:25:10 -0700

    first version works!

--- 8x8x8ArduinoLEDCube.ino
+++ 8x8x8ArduinoLEDCube.ino
@@ -1649,6 +1649,8 @@ randomSeed(analogRead(1));
 
 
 void loop(){//***start loop***start loop***start loop***start loop***start loop***start loop***start loop***start loop***start loop
+  spirals(routineSettings[currentRoutine]);
+  return;
 //Serial.print("START LOOP currentRoutine ");Serial.println(currentRoutine);
 //Each animation located in a sub routine
 // To control an LED, you simply:
@@ -2074,6 +2076,284 @@ void glowingCube(int* settings) {
   }
   clean();
 }
+
+void spirals(int* settings) {
+  int delayFactor = 30*(settings[1]+1);
+  //LED(Y,X,Z,R,G,B);
+  //start by making all faces light up by spiraling in from the outside. Each LED is colored randomly
+  for (int i = 0; i < 8; i++) {//start outer square
+    LED(0,i,0,random(12),random(16),random(16));//front face
+    LED(0,i,7,random(12),random(16),random(16));//back face
+    LED(0,0,i,random(12),random(16),random(16));//left face
+    LED(0,7,i,random(12),random(16),random(16));//right face
+    delay(delayFactor);
+  }
+  for (int i = 1; i < 8; i++) {
+    LED(i,7,0,random(12),random(16),random(16));
+    LED(i,7,7,random(12),random(16),random(16));
+    LED(i,0,7,random(12),random(16),random(16));
+    LED(i,7,7,random(12),random(16),random(16));
+    delay(delayFactor);
+  }
+  for (int i = 6; i >= 1; i--) {
+    LED(7,i,0,random(12),random(16),random(16));
+    LED(7,i,7,random(12),random(16),random(16));
+    LED(7,0,i,random(12),random(16),random(16));
+    LED(7,7,i,random(12),random(16),random(16));
+    delay(delayFactor);
+  }
+  for (int i = 7; i >= 1; i--) {//end outer square
+    LED(i,0,0,random(12),random(16),random(16));
+    LED(i,0,7,random(12),random(16),random(16));
+    LED(i,0,0,random(12),random(16),random(16));
+    LED(i,7,7,random(12),random(16),random(16));
+    delay(delayFactor);
+  }
+  for (int i = 1; i < 7; i++) {//start first inner square
+    LED(1,i,0,random(12),random(16),random(16));//front face
+    LED(1,i,7,random(12),random(16),random(16));//back face
+    LED(1,0,i,random(12),random(16),random(16));//left face
+    LED(1,7,i,random(12),random(16),random(16));//right face
+    LED(0,i,1,random(10),random(16),random(16));//bottom face
+    LED(7,i,1,random(10),random(16),random(16));//top face
+    delay(delayFactor);
+  }
+  for (int i = 1; i < 7; i++) {
+    LED(i,6,0,random(12),random(16),random(16));
+    LED(i,6,7,random(12),random(16),random(16));
+    LED(i,0,6,random(12),random(16),random(16));
+    LED(i,7,6,random(12),random(16),random(16));
+    LED(0,6,i,random(10),random(16),random(16));
+    LED(7,6,i,random(10),random(16),random(16));
+    delay(delayFactor);
+  }
+  for (int i = 6; i > 1; i--) {
+    LED(6,i,0,random(12),random(16),random(16));
+    LED(6,i,7,random(12),random(16),random(16));
+    LED(6,0,i,random(12),random(16),random(16));
+    LED(6,7,i,random(12),random(16),random(16));
+    LED(0,i,6,random(10),random(16),random(16));
+    LED(7,i,6,random(10),random(16),random(16));
+    delay(delayFactor);
+  }
+  for (int i = 6; i > 1; i--) {//end first inner square
+    LED(i,1,0,random(12),random(16),random(16));
+    LED(i,1,7,random(12),random(16),random(16));
+    LED(i,0,1,random(12),random(16),random(16));
+    LED(i,7,1,random(12),random(16),random(16));
+    LED(0,1,i,random(10),random(16),random(16));
+    LED(7,1,i,random(10),random(16),random(16));
+    delay(delayFactor);
+  }
+  for (int i = 2; i < 6; i++) {//start second inner square
+    LED(2,i,0,random(12),random(16),random(16));
+    LED(2,i,7,random(12),random(16),random(16));
+    LED(2,0,i,random(12),random(16),random(16));
+    LED(2,7,i,random(12),random(16),random(16));  
+    LED(0,i,2,random(10),random(16),random(16));
+    LED(7,i,2,random(10),random(16),random(16));  
+    delay(delayFactor);
+  }
+  for (int i = 2; i < 6; i++) {
+    LED(i,5,0,random(12),random(16),random(16));
+    LED(i,5,7,random(12),random(16),random(16));
+    LED(i,0,5,random(12),random(16),random(16));
+    LED(i,7,5,random(12),random(16),random(16));
+    LED(0,5,i,random(10),random(16),random(16));
+    LED(7,5,i,random(10),random(16),random(16));
+    delay(delayFactor);
+  }
+  for (int i = 5; i > 2; i--) {
+    LED(5,i,0,random(12),random(16),random(16));
+    LED(5,i,7,random(12),random(16),random(16));
+    LED(5,0,i,random(12),random(16),random(16));
+    LED(5,7,i,random(12),random(16),random(16));
+    LED(0,i,5,random(10),random(16),random(16));
+    LED(7,i,5,random(10),random(16),random(16));
+    delay(delayFactor);
+  }
+  for (int i = 5; i > 2; i--) {//end second inner square
+    LED(i,2,0,random(12),random(16),random(16));
+    LED(i,2,7,random(12),random(16),random(16));
+    LED(i,0,2,random(12),random(16),random(16));
+    LED(i,7,2,random(12),random(16),random(16));
+    LED(0,2,i,random(10),random(16),random(16));
+    LED(7,2,i,random(10),random(16),random(16));
+    delay(delayFactor);
+  }
+  LED(3,3,0,random(12),random(16),random(16));//start third inner square
+  LED(3,3,7,random(12),random(16),random(16));
+  LED(3,0,3,random(12),random(16),random(16));
+  LED(3,7,3,random(12),random(16),random(16));
+  LED(0,3,3,random(10),random(16),random(16));
+  LED(7,3,3,random(10),random(16),random(16));
+  delay(delayFactor);
+  LED(3,4,0,random(12),random(16),random(16));
+  LED(3,4,7,random(12),random(16),random(16));
+  LED(3,0,4,random(12),random(16),random(16));
+  LED(3,7,4,random(12),random(16),random(16));
+  LED(0,4,3,random(10),random(16),random(16));
+  LED(7,4,3,random(10),random(16),random(16));
+  delay(delayFactor);
+  LED(4,4,0,random(12),random(16),random(16));
+  LED(4,4,7,random(12),random(16),random(16));
+  LED(4,0,4,random(12),random(16),random(16));
+  LED(4,7,4,random(12),random(16),random(16));
+  LED(0,4,4,random(10),random(16),random(16));
+  LED(7,4,4,random(10),random(16),random(16));
+  
+  delay(delayFactor);
+  LED(4,3,0,random(12),random(16),random(16));
+  LED(4,3,7,random(12),random(16),random(16));
+  LED(4,0,3,random(12),random(16),random(16));
+  LED(4,7,3,random(12),random(16),random(16));
+  LED(0,3,4,random(10),random(16),random(16));
+  LED(7,3,4,random(10),random(16),random(16));//end third inner square
+  delay(delayFactor);
+  //now make all the face LEDs sparkle random colors for a bit
+//  for (int loop = 0; loop < 1; loop++) {
+//    for (int i = 0; i < 8; i++) {
+//      for (int j = 0; j < 8; j++) {
+//        LED(i,j,0,random(12),random(16),random(16));
+//        LED(i,j,7,random(12),random(16),random(16));
+//        LED(i,0,j,random(12),random(16),random(16));
+//        LED(i,7,j,random(12),random(16),random(16));
+//        LED(0,i,j,random(10),random(16),random(16));
+//        LED(7,i,j,random(10),random(16),random(16));
+//      }
+//    }
+//  }
+  //Cool, now do the spiral thing again in reverse but change LED colors to 0,0,0 to turn them off. This will make the spiral undo itself from the inside
+  LED(4,3,0,random(0),random(0),random(0));
+  LED(4,3,7,random(0),random(0),random(0));
+  LED(4,0,3,random(0),random(0),random(0));
+  LED(4,7,3,random(0),random(0),random(0));
+  LED(0,3,4,random(0),random(0),random(0));
+  LED(7,3,4,random(0),random(0),random(0));
+  delay(delayFactor);
+  LED(4,4,0,random(0),random(0),random(0));
+  LED(4,4,7,random(0),random(0),random(0));
+  LED(4,0,4,random(0),random(0),random(0));
+  LED(4,7,4,random(0),random(0),random(0));
+  LED(0,4,4,random(0),random(0),random(0));
+  LED(7,4,4,random(0),random(0),random(0));
+  delay(delayFactor);
+  LED(3,4,0,random(0),random(0),random(0));
+  LED(3,4,7,random(0),random(0),random(0));
+  LED(3,0,4,random(0),random(0),random(0));
+  LED(3,7,4,random(0),random(0),random(0));
+  LED(0,4,3,random(0),random(0),random(0));
+  LED(7,4,3,random(0),random(0),random(0));
+  delay(delayFactor);
+  LED(3,3,0,random(0),random(0),random(0));
+  LED(3,3,7,random(0),random(0),random(0));
+  LED(3,0,3,random(0),random(0),random(0));
+  LED(3,7,3,random(0),random(0),random(0));
+  LED(0,3,3,random(0),random(0),random(0));
+  LED(7,3,3,random(0),random(0),random(0));
+  delay(delayFactor);
+  for (int i = 3; i <= 5; i++) {
+    LED(i,2,0,random(0),random(0),random(0));
+    LED(i,2,7,random(0),random(0),random(0));
+    LED(i,0,2,random(0),random(0),random(0));
+    LED(i,7,2,random(0),random(0),random(0));
+    LED(0,2,i,random(0),random(0),random(0));
+    LED(7,2,i,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+  for (int i = 2; i <= 5; i++) {
+    LED(5,i,0,random(0),random(0),random(0));
+    LED(5,i,7,random(0),random(0),random(0));
+    LED(5,0,i,random(0),random(0),random(0));
+    LED(5,7,i,random(0),random(0),random(0));
+    LED(0,i,5,random(0),random(0),random(0));
+    LED(7,i,5,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+  for (int i = 5; i > 2; i--) {
+    LED(i,5,0,random(0),random(0),random(0));
+    LED(i,5,7,random(0),random(0),random(0));
+    LED(i,0,5,random(0),random(0),random(0));
+    LED(i,7,5,random(0),random(0),random(0));
+    LED(0,5,i,random(0),random(0),random(0));
+    LED(7,5,i,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+  for (int i = 5; i > 1; i--) {
+    LED(2,i,0,random(0),random(0),random(0));
+    LED(2,i,7,random(0),random(0),random(0));
+    LED(2,0,i,random(0),random(0),random(0));
+    LED(2,7,i,random(0),random(0),random(0));  
+    LED(0,i,2,random(0),random(0),random(0));
+    LED(7,i,2,random(0),random(0),random(0));  
+    delay(delayFactor);
+  }
+  for (int i = 2; i <= 5; i++) {
+    LED(i,1,0,random(0),random(0),random(0));
+    LED(i,1,7,random(0),random(0),random(0));
+    LED(i,0,1,random(0),random(0),random(0));
+    LED(i,7,1,random(0),random(0),random(0));
+    LED(0,1,i,random(0),random(0),random(0));
+    LED(7,1,i,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+    for (int i = 1; i <= 5; i++) {
+    LED(6,i,0,random(0),random(0),random(0));
+    LED(6,i,7,random(0),random(0),random(0));
+    LED(6,0,i,random(0),random(0),random(0));
+    LED(6,7,i,random(0),random(0),random(0));
+    LED(0,i,6,random(0),random(0),random(0));
+    LED(7,i,6,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+    for (int i = 7; i > 1; i--) {//d
+    LED(i,6,0,random(0),random(0),random(0));
+    LED(i,6,7,random(0),random(0),random(0));
+    LED(i,0,6,random(0),random(0),random(0));
+    LED(i,7,6,random(0),random(0),random(0));
+    LED(0,6,i,random(0),random(0),random(0));
+    LED(7,6,i,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+   for (int i = 6; i > 0; i--) {//d
+    LED(1,i,0,random(0),random(0),random(0));
+    LED(1,i,7,random(0),random(0),random(0));
+    LED(1,0,i,random(0),random(0),random(0));
+    LED(1,7,i,random(0),random(0),random(0));
+    LED(0,i,1,random(0),random(0),random(0));
+    LED(7,i,1,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+    for (int i = 1; i <= 7; i++) {
+    LED(i,0,0,random(0),random(0),random(0));
+    LED(i,0,7,random(0),random(0),random(0));
+    LED(i,0,0,random(0),random(0),random(0));
+    LED(i,7,7,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+    for (int i = 1; i <= 7; i++) {
+    LED(7,i,0,random(0),random(0),random(0));
+    LED(7,i,7,random(0),random(0),random(0));
+    LED(7,0,i,random(0),random(0),random(0));
+    LED(7,7,i,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+    for (int i = 7; i > 0; i--) {
+    LED(i,7,0,random(0),random(0),random(0));
+    LED(i,7,7,random(0),random(0),random(0));
+    LED(i,0,7,random(0),random(0),random(0));
+    LED(i,7,7,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+    for (int i = 7; i > 0; i--) {
+    LED(0,i,0,random(0),random(0),random(0));
+    LED(0,i,7,random(0),random(0),random(0));
+    LED(0,0,i,random(0),random(0),random(0));
+    LED(0,7,i,random(0),random(0),random(0));
+    delay(delayFactor);
+  }
+}
+
 
 void dancingCube(int* settings) {
   clean();
